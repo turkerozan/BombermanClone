0951b3b284f3c1455c2f03abaf5009f6ad402852
Ozan Türker <ozantde@gmail.com>
2017-04-23 22:56:10 +0300

    First Commit
    Created Project

---
+++ BombermanClone/Assets/Scripts/Bomb.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bomb : MonoBehaviour
+{
+
+    public GameObject explodedOne;
+
+    void OnDestroy()
+    {
+        Instantiate(explodedOne, transform.position, Quaternion.identity);
+    }
+}
---
+++ BombermanClone/Assets/Scripts/Explosion.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Explosion : MonoBehaviour {
+
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (!collision.gameObject.isStatic)
+            Destroy(collision.gameObject);
+    }
+}
